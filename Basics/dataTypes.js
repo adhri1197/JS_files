d4114f0917343c02f1f3bc8a9fce833cc0be99b4
Anirudh Vardhan <164553084+adhri1197@users.noreply.github.com>
2024-05-08 18:56:09 +0000

    feat: Add data types and variables examples

---
+++ Basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version ES6
+
+// Primitive Data Types
+let name = "anirudh"; //string
+let age = 25; //number 2 to power 53 
+let isLOggedIn = true; //boolean true or false
+let bigint = 1234567890123456789012345678901234567890n; //bigint
+let salary = 12345.67; //float
+let account = null; //null stands for no value
+let account1; //undefined stands for no value
+let symbol = Symbol("symbol"); //symbol to find unique value
+let object = {}; //object
+
+console.log(typeof null, typeof account1, typeof symbol); //null is object, undefined is undefined, symbol is symbol
