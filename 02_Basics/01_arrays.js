4988d190bbecd85cccef423d8b0ce848c171118d
Anirudh Vardhan <164553084+adhri1197@users.noreply.github.com>
2024-05-12 09:33:29 +0000

    chore: Update array examples and add array methods

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,2 +1,32 @@
-const myArray = [1, 2, 3, 4, 5];
+const myArray = [0, 1, 2, 3, 4, 5];
+
+const myHeros = ['Batman, Superman, Ironman'];
+
+const myNewArray = new Array(0, 1, 2, 3, 4, 5);
+
 console.log(myArray[2]);
+
+
+//Array Methods
+
+myArray.push(6);
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+
+myArray.unshift(9);
+myArray.shift();
+console.log(myArray);
+
+console.log(myArray.includes(7));
+console.log(myArray.indexOf(3))
+
+
+const newArray = myArray.join();
+
+console.log( myArray);
+console.log(newArray);
+
+//slice, splice
+
